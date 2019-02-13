d913f9c8310904ca34bc35d20ce0b32bbc92b422
JhAZZ <JhAZZ@no.com>
2019-02-13 20:04:36 +0700

    updated deploycmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,15 @@
 @rem @SET WWWROOT_PATH=c:\progs\nmp\WWW\asm
-@cls
+@if %WWWROOT_PATH% == "" GOTO :ERROR1
 @set DESTPATH=%WWWROOT_PATH%\gs
+@echo ###############################
+@echo DEPLOYING TO "%DESTPATH%"
 xcopy /Y /D /I ts\*.ts %DESTPATH%\ts
 xcopy /Y /E /D www\*.* %DESTPATH%
 xcopy /Y /E /D /I build\*.* %DESTPATH%\build
+goto :END
+
+:ERROR1
+@echo !DEPLOY ERROR
+@echo You have to set up WWWROOT_PATH environment variable to your WEB root
+
+:END
